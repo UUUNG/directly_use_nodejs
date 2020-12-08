de02268b95d4d12f0e08ea05799bc8dc40d6d908
Ung <jeawoong123@naver.com>
2020-12-08 14:58:33 +0000

    add getting imglink function in folder name of queryData.id. Finally i success! it will change other imgs when add anotehr island

--- main.js
+++ main.js
@@ -1,76 +1,29 @@
 var http = require('http');
-var fs = require('fs');
+const fs = require('fs');
 var url = require('url');
+var template = require('./lib/template.js');
 //var multer = require('multer')
 
-function templatePages(title, list, description, image){
-	return  `
-		<!doctype html>
-		<html>
-			<head>
-			  <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
-			  <title>${title}</title>
-			  <meta charset="utf-8">
-			  <style>
-				a {
-					color: #333333;
-					text-decoration: none;
-				}
-				a:hover{
-					color: #888888;
-				}
-				h1, h2 {
-					text-align : center;
-				}				
-				p { 					
-					font-family: 'Nerko One', cursive;  
-					color: #111; 
-					font-size: 18px;
-				}
-				#jpg{
-					width: 1000px;
-					height: 562px;
-					display: block;
-					margin: 0 auto;					
-				}
-				#explainContainer{					
-					padding: 30px 100px;
-				}
-			  </style>
-			</head>
-			<body>	
- 			  <div id="leftMenuContainer">
-				  ${list}				  			  
-			  </div>
-			  <h1><a href=?id=${title}>${title}</a></h1>
-			  <h2>${title} is beautiful island</h2>
-			  ${image}
-			  <div id="explainContainer">
-				  <p>${description}</p>
-              </div>
-			</body>
-		</html>
-		`;
-}
-
-function makelinks(filelist){
+function makelinks(filelist){ //filelist의 파일이름의 링크를 생성하는 함수
 	var list = '';
 	var i = 0; 
 	while(i < filelist.length){
-		
-		for(var fl in filelist[i]){
-			var temp_list = '';
-			if (fl === ' '){
-				temp_list = temp_list + '';
-			}
-		}
-		list = list + `<a href="/?id=${filelist[i]}">${filelist[i]}</a>
+		list = list + `<a href="/?id=${filelist[i]}">${filelist[i]}</a> 
 `
 		i = i + 1;	  
 	}					
 	return list 
 }
+
+
+function get_imgsrc(Qid, callback) { //이것이 비동기 프로그래밍인가
+    fs.readdir('./img', function(error, filelist){					
+		fs.readFile(`img/${Qid}`, 'utf8', function(err, description){			
+			callback(null, description);									
+		});			
+	});
+}
+
 
 var app = http.createServer(function(request,response){
 	var _url = request.url;
@@ -83,28 +36,28 @@ var app = http.createServer(function(request,response){
 				fs.readFile(`data/${"Malta"}`, 'utf8', function(err, description){
 					var title = 'Welcome';				
 					var list = makelinks(filelist);
-					var html = templatePages(title, list, description, `<img id="jpg" src="https://proxy.goorm.io//service/5fc258f48c477db5e7f801d0_deRM32F9G66dlOErU7n.run.goorm.io/9080//file/load/Malta.jpg?path=d29ya3NwYWNlJTJGbm9kZV9zdHVkeSUyRmRpcmVjdGx5X3VzZV9ub2RlanMlMkZNYWx0YS5qcGc=&docker_id=deRM32F9G66dlOErU7n&secure_session_id=yeIyeh1tQ878YKxWiVLnam4qz0tqdGmh"/>`);
+					var html = template.html(title, list, description, `<img id="jpg" src="https://proxy.goorm.io//service/5fc258f48c477db5e7f801d0_deRM32F9G66dlOErU7n.run.goorm.io/9080//file/load/Malta.jpg?path=d29ya3NwYWNlJTJGbm9kZV9zdHVkeSUyRmRpcmVjdGx5X3VzZV9ub2RlanMlMkZNYWx0YS5qcGc=&docker_id=deRM32F9G66dlOErU7n&secure_session_id=yeIyeh1tQ878YKxWiVLnam4qz0tqdGmh"/>`);
 					response.writeHead(200);
 					response.end(html);
 				});																													
 			})					
-		} else{ //id값이 있는 경우
-			fs.readdir('./img', function(error, filelist){
-				img_srcs = filelist[0];
-			});
+		} else{ //id값이 있는 경우					
+			var imgsrc = '';
+			get_imgsrc(queryData.id, function (err, content) {
+				imgsrc += String(content);				
+			});		
 			fs.readdir('./data', function(error, filelist){											
 				fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
 					var title = queryData.id;	
-					var list = makelinks(filelist);
-					// var upload = multer({ dest: 'uploads/' })
-					// upload.single(title+".jpg");
-					var html = templatePages(title, list, description, `<img id="jpg" src="https://proxy.goorm.io//service/5fc258f48c477db5e7f801d0_deRM32F9G66dlOErU7n.run.goorm.io/9080//file/load/Guam.jpg?path=d29ya3NwYWNlJTJGbm9kZV9zdHVkeSUyRmRpcmVjdGx5X3VzZV9ub2RlanMlMkZpbWclMkZHdWFtLmpwZw==&docker_id=deRM32F9G66dlOErU7n&secure_session_id=z_-7_yShkdkkg1-1-Pb8-NdxvdF6O8SQ"/>`);							
+					var list = makelinks(filelist);				
+					console.log("imgsrc: ", imgsrc);
+					var html = template.html(title, list, description, `<img id="jpg" src=${imgsrc}/>`);							
 					response.writeHead(200);
 					response.end(html);
 				});
 			});
-		}
-		//https://media.timeout.com/images/105240189/1372/772/image.jpg
+			
+		}		
 		
 	}else{		
 		response.writeHead(404);
@@ -114,6 +67,6 @@ var app = http.createServer(function(request,response){
 
 app.listen(3001);
 
-//https://ppgin.run.goorm.io/
+//https://ppgin.run.goorm.io/ 
 
-//pm2 start main.js --watch --ignore-watch="data/* sessions/*"  --no-daemon
+//pm2 start main.js --watch --ignore-watch="data/* sessions/*"  --no-daemon 
